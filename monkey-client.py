f9a7538f3822029b37e198fcd4b8c3fcad660175
Avery Lamp <averylamp@gmail.com>
2020-03-25 10:27:20 -0400

    Adds monkey client file

---
+++ monkey-client.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/ping')
+def ping():
+    return 'pong!'
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9991)
+    
