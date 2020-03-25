b0067034cc01029f7c8b8617b3e24a9ddaa7cdfa
Avery Lamp <averylamp@gmail.com>
2020-03-25 11:30:26 -0400

    Different script installation dependency

--- inject-start.sh
+++ inject-start.sh
@@ -8,5 +8,4 @@ echo -e "Starting forwarding of startup-script to monkey-client.log\n\n"
 mkdir -p /home/monkey
 git clone https://github.com/Averylamp/monkey-client.git /home/monkey/monkey-client
 
-/home/monkey/monkey-client/start.sh 2>&1 | tee /var/log/monkey-client.log &
-disown
+/home/monkey/monkey-client/start.sh &
