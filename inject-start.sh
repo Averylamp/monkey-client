85b98aca03388359828499efdacac73450b3e939
Avery Lamp <averylamp@gmail.com>
2020-03-25 11:01:21 -0400

    updates log scripts

--- inject-start.sh
+++ inject-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tail -f /var/log/syslog | grep "startup-script" >> /var/log/monkey-client.log &
+tail -f /var/log/syslog | grep "startup-script" > /var/log/monkey-client.log 2>&1 &
 
 mkdir -p /home/monkey
 git clone https://github.com/Averylamp/monkey-client.git /home/monkey/monkey-client
 
-/home/monkey/monkey-client/start.sh & >> /var/log/monkey-client.log
+/home/monkey/monkey-client/start.sh & >> /var/log/monkey-client.log 2>&1 &
