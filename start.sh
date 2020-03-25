52fa32b7d90fb16ba59dde4cf12eee098b3fa1fe
Avery Lamp <averylamp@gmail.com>
2020-03-25 11:21:03 -0400

    Different script installation dependency

--- start.sh
+++ start.sh
@@ -2,10 +2,15 @@
 
 # Changes to directory of the script
 cd "$(dirname "$0")"
-apt-get update && apt-get -y install python3-dev
+echo -e "Starting installation of python3-dev venv \n\n"
+apt-get update && apt-get -y install python3-venv
+echo -e "Creating venv \n\n"
 python3 -m venv venv
+echo -e "Activating venv \n\n"
 source venv/bin/activate
 
+echo -e "Installing Requirements \n\n"
 pip install -r requirements.txt
 
+echo -e "Running monkey \n\n"
 python monkey-client.py
